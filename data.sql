d09de81d01057487adbb75ade9e4d90ac03e6f84
Ismail Ali <ismailchangezi801@gmail.com>
2023-05-11 17:10:34 +0500

    Implement the required changes

--- data.sql
+++ data.sql
@@ -4,5 +4,5 @@ SELECT * FROM animals;
 INSERT INTO animals (id,name,date_of_birth,escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon', '2018-11-15', 2, true, 8 ); 
 SELECT * FROM animals;
 UPDATE animals SET neutered = false WHERE id = 1;
-INSERT INTO animals (id,name,date_of_birth,escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', '2021-1-12' , 1, false , 15.04 ), (4, 'Devimon', '2017-5-7', 5, true,11);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', '2021-1-7' , 1, false , 15.04 ), (4, 'Devimon', '2017-5-12', 5, true,11);
 SELECT * FROM animals;
