b9a6e9c4caa0e5531b06ec74b0151788df2fce3c
Ismail Ali <ismailchangezi801@gmail.com>
2023-05-12 18:27:16 +0500

    Requirements for the third project

--- schema.sql
+++ schema.sql
@@ -11,3 +11,32 @@
 -- for the second day's project
 
  ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+--  day 3 project
+
+CREATE TABLE owners(
+    id SERIAL PRIMARY KEY,
+    full_name CHAR(50),
+    age INT
+);
+
+CREATE TABLE species(
+    id SERIAL PRIMARY KEY,
+    name CHAR(50)
+);
+
+CREATE SEQUENCE my_id AS integer START 1 OWNED BY animals.id;
+
+ALTER TABLE animals ALTER COLUMN id SET DEFAULT nextval('my_id');
+
+\d animals;
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN
+species_id INTEGER REFERENCES species(id);
+
+SELECT * FROM animals;
+
+ ALTER TABLE animals ADD COLUMN
+  owner_id INTEGER REFERENCES owners(id);
