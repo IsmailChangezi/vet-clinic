65c03ab4359b0ad2fdf80531f71f6cb57f0babe8
Ismail Ali <ismailchangezi801@gmail.com>
2023-05-13 12:17:37 +0500

    Implement required changes

--- data.sql
+++ data.sql
@@ -20,6 +20,20 @@ SELECT * FROM animals;
 
  SELECT * FROM animals;
 
+ INSERT INTO owners(full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+INSERT INTO species(name)
+VALUES
+  ('Pokemon'),
+  ('Digimon');
+
    insert into vets(name, age, date_of_graduation)
   values
     ('William Tatcher', 45, '2000-04-23'),
--- schema.sql
+++ schema.sql
@@ -41,19 +41,7 @@ SELECT * FROM animals;
  ALTER TABLE animals ADD COLUMN
   owner_id INTEGER REFERENCES owners(id);
 
-INSERT INTO owners(full_name, age)
-VALUES
-  ('Sam Smith', 34),
-  ('Jennifer Orwell', 19),
-  ('Bob', 45),
-  ('Melody Pond', 77),
-  ('Dean Winchester', 14),
-  ('Jodie Whittaker', 38);
 
-INSERT INTO species(name)
-VALUES
-  ('Pokemon'),
-  ('Digimon');
 
 update animals set species_id = case 
   when name like '%mon' then (select id from species where name = 'Digimon')
