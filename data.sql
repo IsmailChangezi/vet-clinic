1308f6a5dad3de745acd33f3104428182067cd81
Ismail Changezi <ismailchangezi801@gmail.com>
2023-05-22 19:26:06 +0500

    Update data.sql

--- data.sql
+++ data.sql
@@ -19,3 +19,7 @@ SELECT * FROM animals;
  (11, 'Ditto', '2022-5-14', 4, 'true', 22);
 
  SELECT * FROM animals;
+ 
+ INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+ 
+ insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
